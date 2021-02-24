a5a2792b4ad550e73910e3b7674597df0df6742d
Denis Saenko <dsaenko@yandex.ru>
2021-02-24 21:34:33 +0100

    turnstyle test

--- src/main/java/com/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/example/helloworld/HelloWorldApplication.java
@@ -20,6 +20,7 @@ public class HelloWorldApplication {
         }
     }
 	public static void main(String[] args) {
+        System.out.println("a");
         SpringApplication.run(HelloWorldApplication.class, args);
 	}
 
